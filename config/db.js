681b61b05583af508753140bf884f3e67c45ad55
Gianpiero Porchia <gianpiero@porchia.it>
2018-04-08 21:31:26 +0200

    v2 Addeded all the files

---
+++ config/db.js
@@ -0,0 +1,6 @@
+// Set the connection string based from the config vars of the production server
+// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
+
+module.exports = {
+	db: 'mongodb://porchiaTest01_02:MongoDB2018!!!@porchiatest01-shard-00-00-t10gy.gcp.mongodb.net:27017,porchiatest01-shard-00-01-t10gy.gcp.mongodb.net:27017,porchiatest01-shard-00-02-t10gy.gcp.mongodb.net:27017/test?ssl=true&replicaSet=PorchiaTest01-shard-0&authSource=admin'
+};
